2b785f598e28f286687554fd85234dc036ca6b91
doguhanyeke <doguyeke@gmail.com>
2020-09-09 22:27:02 +0300

    4.22.

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 /*
 api variable is a superobject which enables
@@ -12,17 +13,38 @@ const api = supertest(app)
 /*
 beforeEach is Jest function
 */
+let token = ""
 beforeEach(async () => {
     await Blog.deleteMany({})
+    console.log("blogs deleted")
+    await User.deleteMany({})
+    console.log("users deleted")
 
+    const user = {
+      username: "two",
+      name: "dido",
+      password: "gizli"
+    }
+    let result = await api.post('/api/users').send(user)
+    console.log("1 user added")
+
+    result = await api.post('/api/login').send({
+      username: "two",
+      password: "gizli"
+    })
+    
+    token = result.body.token
+    
     await api
     .post('/api/blogs')
+    .set({'Authorization': `bearer ${token}`})
     .send({
       title: "asd",
       author: "dogu",
       url: "url.co",
       likes: 5
     })
+    console.log("1 blog created")
 })
 
 test('GET request works', async () => {
@@ -38,6 +60,7 @@ test('POST request works', async () => {
 
   await api
     .post('/api/blogs')
+    .set({"Authorization": `bearer ${token}`})
     .send({
       title: "asd",
       author: "dogu",
@@ -55,6 +78,7 @@ test('_id does not appear', async () => {
     .get('/api/blogs')
     .expect(200)
   const body = blogs.body
+  console.log("bodi", body)
   expect(body[0]._id).not.toBeDefined()
 })
 
@@ -69,6 +93,7 @@ test('id does appear', async () => {
 test('when property of like missed, it defaulted to 0', async () => {
   await api
   .post('/api/blogs')
+  .set({"Authorization": `bearer ${token}`})
   .send({
     title: "temp",
     author: "doguh",
@@ -98,6 +123,7 @@ test('title and url missed', async () => {
 test('delete a blog', async () => {
   await api
   .post('/api/blogs')
+  .set({"Authorization": `bearer ${token}`})
   .send({
     author: "doguh",
     title: "tit",
@@ -114,7 +140,9 @@ test('delete a blog', async () => {
   let blogTemp = blogBodies.find(blog => blog.title === 'tit')
   const idOfBlog = blogTemp.id
 
-  await api.delete(`/api/blogs/${idOfBlog}`)
+  await api
+    .delete(`/api/blogs/${idOfBlog}`)
+    .set({"Authorization": `bearer ${token}`})
 
   blogs = await api
     .get('/api/blogs')
@@ -135,6 +163,23 @@ test('update a post', async () => {
   await api.put(`/api/blogs/${soonUpdatedBlog.id}`)
   .send({likes: 1001001})
   .expect(200)
+})
+
+test('token is not provided, Unauthorized', async () => {
+  const blog = await api
+  .post('/api/blogs')
+  .set({"Authorization": `bearer ${token}`})
+  .send({
+    author: "doguh",
+    title: "tit",
+    author: "dy",
+    likes: 3
+  })
+
+  await api
+    .delete(`/api/blogs/${blog.id}`)
+    .expect(401)
+    
 })
 
 afterAll(() => {
