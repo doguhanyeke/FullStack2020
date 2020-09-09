9119453e0bb2482ae328aa09bdd85610364c20d4
doguhanyeke <doguyeke@gmail.com>
2020-09-09 19:18:12 +0300

    4.18.

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const cors = require('cors')
 const blogRouter = require('./controllers/bloglist')
 const userRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
@@ -20,6 +21,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownendpoint)
 app.use(middleware.errorHandler)
 
---
+++ part4/controllers/login.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (req, res ) => {
+    const body = req.body
+
+    const uname = body.username
+    const pword = body.password
+
+    const user = await User.findOne({username: uname})
+    console.log(user)
+
+    console.log(uname, pword, user.name, user.passwordHash)
+    const passwordCorrect = user === null ? false 
+        : await bcrypt.compare(pword, user.passwordHash)
+    
+    console.log(passwordCorrect)
+    if(!passwordCorrect){
+        return res.status(401).json({error: 'invalid username or password'})
+    }
+
+    const userToken = {
+        username: uname,
+        id: user._id
+    }
+
+    const token = jwt.sign(userToken, process.env.SECRET)
+
+    res.status(200).send(
+        {token: token, username: uname, name: user.name}
+    )
+})
+
+module.exports = loginRouter
