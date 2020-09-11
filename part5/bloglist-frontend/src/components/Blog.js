7cb1a3609802a6919b256a230bd9bb88692fb7a5
doguhanyeke <doguyeke@gmail.com>
2020-09-11 21:55:28 +0300

    5.8.

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import axios from 'axios'
+
 const Blog = ({ blog }) => {
   const blogStyle = {
     paddingTop: 10,
@@ -8,27 +10,41 @@ const Blog = ({ blog }) => {
     marginBottom: 5
   }
   const [detailVisible, setdetailVisible] = useState(false)
+  const [currentBlog, setCurrentBlog] = useState(blog)
+  
   const showDetails = {display: detailVisible ? '' : 'none'}
 
-  console.log(blog)
-  
-  console.log(blog.user)
+  const increaseLikeofBlog = async () => {
+    const newBlog = {
+      user: currentBlog.user ? currentBlog.user._id : '',
+      likes: currentBlog.likes ?  + currentBlog.likes + 1 : 1,
+      author: currentBlog.author,
+      title: currentBlog.title,
+      url: currentBlog.url
+    }
+    const config = {
+      headers: {Authorization: window.localStorage.getItem('userToken')}
+    }
+    const response = await axios.put(`/api/blogs/${currentBlog.id}`, newBlog, config)
+    setCurrentBlog(response.data)
+  }
+
   return(
     <div style={blogStyle}>
-      {blog.title} {blog.author}
+      {currentBlog.title} {currentBlog.author}
       <button onClick={() => setdetailVisible(true)}>view</button>
       <div style={showDetails}>
         <ul>
           <li>
-            {blog.url}
+            {currentBlog.url}
             <button onClick={() => setdetailVisible(false)}>hide</button>
           </li>
           <li>
-            {blog.likes}
-            <button>like</button>
+            {currentBlog.likes}
+            <button onClick={increaseLikeofBlog}>like</button>
           </li>
           <li>
-            {blog.user ? blog.user.name : null}
+            {currentBlog.user ? currentBlog.user.name : null}
           </li>
         </ul>
       </div>
