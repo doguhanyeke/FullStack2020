7f89e84f0574faf8a7f32f3b23d6d019a59ed2e8
doguhanyeke <doguyeke@gmail.com>
2020-09-16 11:41:39 +0300

    5.15 isnt done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
-import { fireEvent, prettyDOM} from '@testing-library/dom'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
-import CreateForm from './CreateForm'
+import userEvent from '@testing-library/user-event'
 
 test('Blog author and title displayed, but url and likes not', () => {
   const blog = {
@@ -46,47 +46,23 @@ test('Props should be called twice', () => {
     title: 'test title',
     author: 'test author',
     url: 'test url',
-    likes: 0
   }
   const userId = '5f5bdfb98a0ab6f55910ebb9'
   const mockHandler = jest.fn()
   const component = render(
     <Blog blog={blog} userId={userId} updateBlogs={mockHandler}/>
   )
-
-  const viewButton = component.container.querySelector('.viewHideButton')
-  fireEvent.click(viewButton)
-
+  /*
   const likeButton = component.container.querySelector('.likeButton')
   fireEvent.click(likeButton)
   fireEvent.click(likeButton)
-  /*
+  */
+
+  const likeButton = component.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
   console.log(prettyDOM(component.container.querySelector('.blog')))
-    */
+
   expect(mockHandler.mock.calls).toHaveLength(2)
-})
-
-test('Form takes correct inputs', () => {
-  const mockHandler1 = jest.fn()
-  const mockHandler2 = jest.fn()
-  const component = render(
-    <CreateForm createNewNote={mockHandler1} setPostMessage={mockHandler2} />
-  )
-  let input = component.container.querySelector('.titleInput')
-  fireEvent.change(input, {
-    target: { value: 'test titssss' }
-  })
-  input = component.container.querySelector('.authorInput')
-  fireEvent.change(input, {
-    target: { value: 'test autttts' }
-  })
-  input = component.container.querySelector('.urlInput')
-  fireEvent.change(input, {
-    target: { value: 'test urllsss' }
-  })
-  const form = component.container.querySelector('.form')
-  fireEvent.submit(form)
-
-  const author = component.container.querySelector('.titleInput')
-  expect(author.value).toBe('test titssss')
 })
