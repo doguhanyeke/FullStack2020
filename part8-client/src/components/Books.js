23e481a71395f7f06930bd52ee357787d391f218
doguhanyeke <doguyeke@gmail.com>
2020-10-16 19:54:52 +0300

    8.22 already done.

--- part8-client/src/components/Books.js
+++ part8-client/src/components/Books.js
@@ -41,7 +41,9 @@ const Books = (props) => {
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>
+              title
+            </th>
             <th>
               author
             </th>
