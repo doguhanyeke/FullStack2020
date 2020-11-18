82f0a2aaec0b62ccb7965e6f41bcf0834ccb1ad3
doguhanyeke <doguyeke@gmail.com>
2020-11-18 21:43:31 +0300

    9.9

--- part9/partc/build/index.js
+++ part9/partc/build/index.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+app.use(cors_1.default());
 app.use(express_1.default.json());
 const PORT = 3000;
-app.get('/ping', (req, res) => {
+app.get('/api/ping', (req, res) => {
     console.log("here", req);
     res.send("pong!");
 });
--- part9/partc/index.ts
+++ part9/partc/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
 
-app.get('/ping', (req, res) => {
+app.get('/api/ping', (req, res) => {
     console.log("here", req);
     res.send("pong!");
 });
