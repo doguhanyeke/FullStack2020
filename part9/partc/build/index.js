2f1dfda4255bc8c5c9905568b8fea80a6c35f5d5
doguhanyeke <doguyeke@gmail.com>
2020-11-20 13:08:44 +0300

    9.10.

--- part9/partc/build/index.js
+++ part9/partc/build/index.js
@@ -5,14 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const diagnoses_1 = __importDefault(require("./routers/diagnoses"));
 const app = express_1.default();
 app.use(cors_1.default());
 app.use(express_1.default.json());
+app.use("/api/diagnoses", diagnoses_1.default);
 const PORT = 3000;
-app.get('/api/ping', (req, res) => {
-    console.log("here", req);
-    res.send("pong!");
-});
 app.listen(PORT, () => {
     console.log("Listening port: ", PORT);
 });
--- part9/partc/src/index.ts
+++ part9/partc/src/index.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routers/diagnoses';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/diagnoses", diagnoseRouter);
+
 const PORT = 3000;
-
-app.get('/api/ping', (_req, res) => {
-    console.log("here");
-    res.send("pong!");
-});
-
 app.listen(PORT, () => {
     console.log("Listening port: ", PORT);
 });
